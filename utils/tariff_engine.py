c32350289902e75a00e0f007881f422b0e4b4f30
pratik4452 <pratikkhanorkar17@gmail.com>
2026-05-22 16:07:23 +0530

    Update tariff_engine.py

--- utils/tariff_engine.py
+++ utils/tariff_engine.py
@@ -1,1 +1,111 @@
-# Tariff Engine Module
+def calculate_bill_estimation(units):
+
+    result = {}
+
+    try:
+
+        units = float(units)
+
+        # ---------------------------------
+        # ENERGY CHARGES
+        # ---------------------------------
+
+        energy_rate = 8.50
+
+        energy_charges = (
+            units * energy_rate
+        )
+
+        # ---------------------------------
+        # FAC CHARGES
+        # ---------------------------------
+
+        fac_rate = 1.00
+
+        fac_charges = (
+            units * fac_rate
+        )
+
+        # ---------------------------------
+        # WHEELING CHARGES
+        # ---------------------------------
+
+        wheeling_rate = 0.75
+
+        wheeling_charges = (
+            units * wheeling_rate
+        )
+
+        # ---------------------------------
+        # ELECTRICITY DUTY
+        # ---------------------------------
+
+        duty_rate = 0.075
+
+        electricity_duty = (
+            energy_charges * duty_rate
+        )
+
+        # ---------------------------------
+        # FIXED CHARGES
+        # ---------------------------------
+
+        fixed_charges = 5000
+
+        # ---------------------------------
+        # TOTAL BILL
+        # ---------------------------------
+
+        total_bill = (
+
+            energy_charges
+            + fac_charges
+            + wheeling_charges
+            + electricity_duty
+            + fixed_charges
+
+        )
+
+        # ---------------------------------
+        # STORE RESULTS
+        # ---------------------------------
+
+        result["Units"] = round(units, 2)
+
+        result["Energy Charges"] = round(
+            energy_charges,
+            2
+        )
+
+        result["FAC Charges"] = round(
+            fac_charges,
+            2
+        )
+
+        result["Wheeling Charges"] = round(
+            wheeling_charges,
+            2
+        )
+
+        result["Electricity Duty"] = round(
+            electricity_duty,
+            2
+        )
+
+        result["Fixed Charges"] = round(
+            fixed_charges,
+            2
+        )
+
+        result["Estimated Bill"] = round(
+            total_bill,
+            2
+        )
+
+    except:
+
+        result["Error"] = (
+            "Tariff Calculation Error"
+        )
+
+    return result
